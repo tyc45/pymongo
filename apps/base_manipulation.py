c6abd724f1da052e1d1650a61ff80f28a807d440
Jordan Tyc <tyc450@gmail.com>
2021-12-10 09:25:44 +0100

    created access to db

--- apps/base_manipulation.py
+++ apps/base_manipulation.py
@@ -2,4 +2,15 @@ import streamlit as st
 import pandas as pd
 import pymongo
 
+client = pymongo.MongoClient(**st.secrets["mongo"])
+
+@st.cache(ttl=600)
+def get_data():
+    db = client.carsDB
+    item = db.carDataset.find()
+    item = list(item)  # make hashable for st.cache
+    return item
+
+cars = get_data()
+
 st.title("You like cars? I like cars!")
